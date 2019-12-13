f259680dc58384686ce5579211ef37d39cfc3f09
Tomasz Tunik <tomasztunik@gmail.com>
2019-12-13 15:07:47 +0100

    update testConnection to drop tables before running tests
    - fixes test errors when testing against local/persistent db

--- spec/utils/testConnection.ts
+++ spec/utils/testConnection.ts
@@ -5,6 +5,7 @@ let connection: Connection;
 
 export async function connect () {
     connection = await createConnection();
+    await connection.synchronize(true);
 }
 
 export function close () {
