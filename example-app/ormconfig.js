78fb6f32d6274cc0c464a745b81ff84accc4e53d
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-08-03 21:15:27 +0200

    fix: wrong publication setting

---
+++ example-app/ormconfig.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+const rootDir = 'build/src'
+
+module.exports = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: +(process.env.POSTGRES_PORT || 5432),
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || '',
+  database: process.env.POSTGRES_DATABASE || 'database_test',
+  entities: [path.join(rootDir, '/entity/**/*.js')],
+  migrations: [path.join(rootDir, '/migration/**/*.js')],
+  subscribers: [path.join(rootDir, '/subscriber/**/*.js')],
+  synchronize: true,
+  logging: false,
+  cli: {
+    migrationsDir: path.join(rootDir, '/migration'),
+    entitiesDir: path.join(rootDir, '/entity'),
+    subscribersDir: path.join(rootDir, '/subscriber'),
+  },
+}
--- example-app/src/index.ts
+++ example-app/src/index.ts
@@ -1,10 +1,17 @@
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
 import express from 'express'
 import AdminBro from '@admin-bro/core'
 import { buildRouter } from '@admin-bro/express'
+import TypeormAdapter from '@admin-bro/typeorm'
+import { User } from './entity/User'
+
+AdminBro.registerAdapter(TypeormAdapter)
 
 const PORT = 3000
 
 const run = async () => {
+  await createConnection()
   const app = express()
   const admin = new AdminBro()
   const router = buildRouter(admin)
