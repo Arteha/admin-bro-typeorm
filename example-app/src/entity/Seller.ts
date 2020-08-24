5a9dcb3d43401df1c73f68ea14bdfb2ab14e848e
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-08-24 11:05:57 +0200

    test: add new entity to example app

---
+++ example-app/src/entity/Seller.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
+import { Car } from './Car'
+
+export enum UserRoles {
+  DESIGNER = 'designer',
+  CLIENT = 'client'
+}
+
+@Entity()
+export class Seller extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @OneToMany((type) => Car, (car) => car.seller)
+  cars: Array<Car>
+}
