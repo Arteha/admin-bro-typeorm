69b3a72a19f2084787fe92ded594911a3ec4b6b8
Rafał Dzięgielewski <rafal.dzieg@gmail.com>
2022-02-24 11:04:35 +0100

    fix: handle UUID type in filters
    Resolves https://github.com/SoftwareBrothers/adminjs-typeorm/issues/32

--- src/utils/filter/default-filter.parser.ts
+++ src/utils/filter/default-filter.parser.ts
@@ -1,7 +1,14 @@
 import { Like } from 'typeorm'
 import { FilterParser } from './filter.types'
 
+const uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-[5|4|3|2|1][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+
 export const DefaultParser: FilterParser = {
   isParserForType: (filter) => filter.property.type() === 'string',
-  parse: (filter, fieldKey) => ({ filterKey: fieldKey, filterValue: Like(`%${filter.value}%`) }),
+  parse: (filter, fieldKey) => {
+    if (uuidRegex.test(filter.value.toString())) {
+      return { filterKey: fieldKey, filterValue: filter.value }
+    }
+    return { filterKey: fieldKey, filterValue: Like(`%${filter.value}%`) }
+  },
 }
