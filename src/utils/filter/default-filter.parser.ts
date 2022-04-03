96f4b3f35d8ccdfc6fbda59cdc1fd96ba2af9a74
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2022-04-03 09:16:36 +0200

    fix: fix uuid casting for filters

--- src/utils/filter/default-filter.parser.ts
+++ src/utils/filter/default-filter.parser.ts
@@ -1,4 +1,5 @@
-import { Like } from 'typeorm'
+import { Like, Raw } from 'typeorm'
+import { Property } from '../../Property'
 import { FilterParser } from './filter.types'
 
 const uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-[5|4|3|2|1][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
@@ -6,8 +7,14 @@ const uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-[5|4|3|2|1][0-9A-F]{3}-[89AB][0-9A-F
 export const DefaultParser: FilterParser = {
   isParserForType: (filter) => filter.property.type() === 'string',
   parse: (filter, fieldKey) => {
-    if (uuidRegex.test(filter.value.toString())) {
-      return { filterKey: fieldKey, filterValue: filter.value }
+    if (uuidRegex.test(filter.value.toString()) || (filter.property as Property).column.type === 'uuid') {
+      return {
+        filterKey: fieldKey,
+        filterValue: Raw(
+          (alias) => `CAST(${alias} AS text) = :value`,
+          { value: filter.value },
+        ),
+      }
     }
     return { filterKey: fieldKey, filterValue: Like(`%${filter.value}%`) }
   },
